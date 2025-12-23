80cbaae48fc2346390ccd16bbeba6e9fde5b009d
Emmanuel Almonte <emmansa00@gmail.com>
2025-12-24 00:28:19 +1100

    init commit

---
+++ ios/FitView-IOS/FitView-IOS/FitView_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitView_IOSApp.swift
+//  FitView-IOS
+//
+//  Created by Emmanuel Almonte on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitView_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/FitView-IOS/FitView-IOSTests/FitView_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitView_IOSTests.swift
+//  FitView-IOSTests
+//
+//  Created by Emmanuel Almonte on 23/12/2025.
+//
+
+import Testing
+@testable import FitView_IOS
+
+struct FitView_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
